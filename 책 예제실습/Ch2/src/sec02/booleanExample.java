abf3c1cd4a3bf5c8a1da6d3d08fc3cd211e8e09a
piljun kim <kpj091441@gmail.com>
2023-11-02 21:34:17 +0900

    section2-2

---
+++ 책 예제실습/Ch2/src/sec02/booleanExample.java
@@ -0,0 +1,14 @@
+package sec02;
+
+public class booleanExample {
+	public static void main(String[] args) {
+		
+	boolean stop = true;
+	if(stop) {
+		System.out.println("정지합니다.");
+	} else {
+		System.out.println("시작합니다.");
+	}
+	}
+
+}
