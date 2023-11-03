bf07018be38694b46a3ac3c42c539292ed2093bd
piljun kim <kpj091441@gmail.com>
2023-11-03 19:24:29 +0900

    section2-4

---
+++ 책 예제실습/Ch2/src/sec04/QstopExample.java
@@ -0,0 +1,17 @@
+package sec04;
+
+public class QstopExample {
+	public static void main(String[] args) throws Exception{
+		int keyCode;
+		
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode: " + keyCode);
+			if(keyCode == 113) {
+				break;
+			}
+		}
+		System.out.println("종료");
+	}
+
+}
