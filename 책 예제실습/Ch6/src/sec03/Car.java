757737889022250b64be4673ceae73b14b220a76
piljun kim <kpj091441@gmail.com>
2023-11-07 21:36:46 +0900

    section6-3

---
+++ 책 예제실습/Ch6/src/sec03/Car.java
@@ -0,0 +1,8 @@
+package sec03;
+
+public class Car {
+	Car(String color, int cc){
+		
+	}
+
+}
---
+++ 책 예제실습/Ch6/src/sec03/CarExam.java
@@ -0,0 +1,8 @@
+package sec03;
+
+public class CarExam {
+	public static void main(String[] args) {
+		Car myCar = new Car("검정", 3000);
+	}
+
+}
