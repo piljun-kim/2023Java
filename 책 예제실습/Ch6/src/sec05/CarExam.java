f7a9cd78c31ea15421c2ac20591bd3391c5361e0
piljun kim <kpj091441@gmail.com>
2023-11-08 19:00:52 +0900

    section6-5

---
+++ 책 예제실습/Ch6/src/sec05/CarExam.java
@@ -0,0 +1,12 @@
+package sec05;
+
+public class CarExam {
+	public static void main(String[] args) {
+		Car myCar = new Car("포르쉐");
+		Car yourCar = new Car("벤츠");
+		
+		myCar.run();
+		yourCar.run();
+	}
+
+}
---
+++ 책 예제실습/Ch6/src/sec05/EarthExam.java
@@ -0,0 +1,9 @@
+package sec05;
+
+public class EarthExam {
+	public static void main(String[] args) {
+		System.out.println("지구의 반지름: " + Earth.EARTH_RADIUS + " km");
+		System.out.println("지구의 표면적: " + Earth.EARTH_AREA + " km^2");
+	}
+
+}
