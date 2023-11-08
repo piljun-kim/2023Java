af55ccb17fd14c6d8b3dacb5af812232e3e68562
piljun kim <kpj091441@gmail.com>
2023-11-08 20:27:42 +0900

    section7-2

---
+++ 책 예제실습/Ch7/src/sec02/ChildExam.java
@@ -0,0 +1,13 @@
+package sec02;
+
+public class ChildExam {
+	public static void main(String[] args) {
+		Child child = new Child();
+		
+		Parent parent = child;
+		parent.method1();
+		parent.method2();
+		//parent.method3();
+	}
+
+}
