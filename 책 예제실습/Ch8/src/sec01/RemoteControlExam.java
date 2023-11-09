264f400f2b6b4fc337975c161c88a7d344e6132f
piljun kim <kpj091441@gmail.com>
2023-11-09 21:23:19 +0900

    section8-1

---
+++ 책 예제실습/Ch8/src/sec01/RemoteControlExam.java
@@ -0,0 +1,10 @@
+package sec01;
+
+public class RemoteControlExam {
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+
+}
---
+++ 책 예제실습/Ch8/src/sec01/SmartTelevisionExam.java
@@ -0,0 +1,11 @@
+package sec01;
+
+public class SmartTelevisionExam {
+	public static void main(String[] args) {
+		SmartTelevision tv = new SmartTelevision();
+		
+		RemoteControl rc = tv;
+		Searchable searchable = tv;
+	}
+
+}
