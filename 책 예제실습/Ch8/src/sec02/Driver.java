4410aabaa3d54e76c38fba422f4ec22d7a864fea
piljun kim <kpj091441@gmail.com>
2023-11-10 11:21:19 +0900

    section8-2

--- 책 예제실습/Ch8/src/sec02/Driver.java
+++ 책 예제실습/Ch8/src/sec02/Driver.java
@@ -2,6 +2,10 @@ package sec02;
 
 public class Driver {
 	public void drive(Vehicle vehicle) {
+		if(vehicle instanceof Bus) {
+			Bus bus = (Bus) vehicle;
+			bus.checkFare();
+		}
 		vehicle.run();
 	}
 
