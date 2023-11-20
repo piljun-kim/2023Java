427fd3ba8c15527f7e195753549ee63e21424d42
piljun-kim <147688874+piljun-kim@users.noreply.github.com>
2023-11-21 08:56:58 +0900

    .

---
+++ Ch10/src/ExceptionTest/DivideZero.java
@@ -0,0 +1,11 @@
+package ExceptionTest;
+
+public class DivideZero {
+	void method() {
+		int a = 1;
+		int b = 0;
+		int c = a/b;
+			
+	}
+
+}
---
+++ Ch10/src/ExceptionTest/Main.java
@@ -0,0 +1,12 @@
+package ExceptionTest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DivideZero d = new DivideZero();
+		d.method();
+
+	}
+
+}
